b125ff4c273db650450e80b34edc75695b1471ef
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2024-08-24 20:47:59 +0400

    fix User schema typos

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,10 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
-  userName: { type: String, unique: true, required: true },
+  username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   displayName: { type: String, required: true },
-  subscribedEvents: [{ type: Schema.type.ObjectId, ref: "Event" }],
+  subscribedEvents: [{ type: Schema.Types.ObjectId, ref: "Event" }],
 });
 
 export default mongoose.model("User", userSchema);
