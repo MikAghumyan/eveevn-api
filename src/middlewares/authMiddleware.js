e90520c57da7c7a37bb9dbcf81ac35bc5141f8f7
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2024-08-26 15:15:27 +0400

    add authorization middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+
+import User from "../models/User.js";
+
+export default asyncHandler(async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").split(" ")[1];
+
+    if (!token) {
+      res.status(401);
+      throw new Error("Access denied");
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("password");
+
+    if (!user) {
+      res.status(401);
+      throw new Error("Invalid UserId");
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+});
