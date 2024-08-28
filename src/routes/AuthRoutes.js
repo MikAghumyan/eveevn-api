ec2e238e79f3523df1dfa437aee39e10ee054013
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2024-08-28 23:13:51 +0400

    add verify middleware

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
 
+import authMiddleware from "../middlewares/authMiddleware.js";
+
 import { createUser, loginUser } from "../controllers/authController.js";
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
+router.get("/verify", authMiddleware, (req, res) => {
+  res.status(200).json(res.locals.user);
+});
 
 export default router;
